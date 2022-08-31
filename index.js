5c4111a0b920a1a80ef7ea5c7ccd6a3ce5dd9736
Vicky Tang <110417566+vickyT17@users.noreply.github.com>
2022-08-31 22:05:28 +0000

    worked on step 4 and 5

--- index.js
+++ index.js
@@ -15,9 +15,19 @@ let step = 1;
 window.onload = function() {
     createNewInput();
 
+    const graphDiv = document.getElementById('graph')
+
+    graphDiv.onmouseover = () => {
+        document.body.style.cursor = "pointer";
+    }
+
+    graphDiv.onmouseleave = () => {
+        document.body.style.cursor = "default";
+    }
+
     // Graph Javascript
     Graph = ForceGraph()
-        (document.getElementById('graph'))
+        (graphDiv)
 
     Graph.graphData(data)
         .nodeId('id')
@@ -94,6 +104,13 @@ window.onload = function() {
                 ctx.fillText(link.rate ?? "", (link.target.x + link.source.x) / 2, (link.target.y + link.source.y) / 2);
             }
         })
+        .onNodeHover((node) => {
+            if (node === null) {
+                document.body.style.cursor = "pointer";
+            } else {
+                document.body.style.cursor = "grab";
+            }
+        })
         .d3Force('charge', null)
         .d3Force('center', null)
         .d3Force('collide', d3.forceCollide(NODE_RADIUS / 2))
@@ -115,6 +132,7 @@ window.onload = function() {
 }
 
 function handleNext() {
+    const formTitle = document.getElementById("formTitle");
     if (step === 1) {
         step1Next();
     } else if (step === 2) {
@@ -131,15 +149,23 @@ function handleNext() {
         const step3 = document.getElementById("step3-container");
         step2.style.display = "none";
         step3.style.display = "flex";
-        renderStep3();
+        formTitle.innerHTML = "Data Input (Step 3)"
+        if (data.links.filter(link => link.inducer === undefined).length === 0) {
+            handleNext();
+        } else {
+            renderStep3();
+        }
     } else if (step === 3) {
+        step3Next();
+    } else if (step === 4) {
         //step3Next();
         step++;
-        const step3 = document.getElementById("step3-container");
         const step4 = document.getElementById("step4-container");
-        step3.style.display = "none";
-        step4.style.display = "flex";
-        renderStep4();
+        const step5 = document.getElementById("step5-container");
+        step4.style.display = "none";
+        step5.style.display = "flex";
+        formTitle.innerHTML = "Data (Step 5)";
+        renderStep5();
     }
 }
 
@@ -162,7 +188,20 @@ function handleBack() {
         const step4 = document.getElementById("step4-container");
         step3.style.display = "flex";
         step4.style.display = "none";
+        if (data.links.filter(link => link.inducer === undefined).length === 0) {
+            handleBack();
+        }
+    } else if (step === 5) {
+        step--;
+        const step4 = document.getElementById("step4-container");
+        const step5 = document.getElementById("step5-container");
+        step4.style.display = "flex";
+        step5.style.display = "none";
+        handleBack();
     }
+
+    const formTitle = document.getElementById("formTitle");
+    formTitle.innerHTML = "Data Input (Step " + step + ")";
 }
 
 
@@ -249,7 +288,7 @@ function createNewInput() {
                 y: 0
             })
             // temporarily sets the collision force to the whole node radius so that nodes do not intersect on creation
-            Graph.d3Force('collide', d3.forceCollide(NODE_RADIUS))
+            Graph.d3Force('collide', d3.forceCollide(NODE_RADIUS * 1.2))
             Graph.graphData(data);
             setTimeout(() => Graph.d3Force('collide', d3.forceCollide(NODE_RADIUS / 2)), 400)
             // set up a new blank input box 
@@ -257,6 +296,9 @@ function createNewInput() {
         } else {
             // find the pre-existing node by id
             let node = data.nodes.find(x => x.id === button.count);
+            // remove any red border
+            inpt.classList.remove("border");
+            inpt.classList.remove("border-danger");
             // updates the name to the new value in the input
             node.name = e.target.value;
             Graph.graphData(data);
@@ -267,7 +309,7 @@ function createNewInput() {
 // validating step 1 nodes and proceeding to step 2 if all is good
 function step1Next() {
     // create empty set that will hold duplicate / empty nodes
-    let badState = new Set(); 
+    let badState = new Set();
     for (let i = 0; i < data.nodes.length; i++) {
         // check for duplicate state names
         for (let j = i + 1; j < data.nodes.length; j++) {
@@ -300,6 +342,7 @@ function step1Next() {
         element.style.display = "none";
         // sorts the nodes alphabetically 
         data.nodes.sort((a, b) => a.name.localeCompare(b.name));
+        document.getElementById("formTitle").innerHTML = "Data Input (Step 2)";
         renderStep2();
     }
 }
@@ -342,7 +385,7 @@ function renderStep2() {
         accordionButton.setAttribute("data-bs-target", "#panels-collapse-" + data.nodes[i].id);
         accordionButton.ariaExpanded = i === 0 ? "true" : "false";
         accordionButton.setAttribute("aria-controls", "panels-collapse-" + data.nodes[i].id);
-        accordionButton.innerHTML = "Links to State " + data.nodes[i].name + ":"
+        accordionButton.innerHTML = "Links from State " + data.nodes[i].name + ":"
 
         const accordionPanel = document.createElement("div");
         accordionPanel.id = "panels-collapse-" + data.nodes[i].id;
@@ -421,8 +464,9 @@ function renderStep2() {
 function renderStep3() {
     // delete anything that existed in step3-container
     const step3 = document.getElementById("step3-container");
-    const stepTitle = document.createElement("h2");
-    stepTitle.innerHTML = "Input Transition Rates"
+    const stepTitle = document.createElement("h3");
+    stepTitle.className = "title";
+    stepTitle.innerHTML = "Node-Based Transition Rates"
     step3.replaceChildren(stepTitle);
 
     // loop through all data.links and create input fields for transition rates
@@ -446,6 +490,7 @@ function renderStep3() {
         linkInput.className = "form-control";
         linkInput.placeholder = "Transition Rate";
         linkInput.ariaLabel = "Transition Rate";
+        linkInput.id = "link-input-" + data.links[i].source.id + "-" + data.links[i].target.id;
         linkInput.value = data.links[i].rate ?? '';
 
         // add an event listener to detect when the user types something in the input
@@ -454,6 +499,8 @@ function renderStep3() {
         function updateValue(e) {
             // Find and update the corresponding link
             data.links[i].rate = e.target.value;
+            linkInput.classList.remove("border");
+            linkInput.classList.remove("border-danger");
             // Updating the graph
             Graph.graphData(data);
         }
@@ -469,23 +516,23 @@ function renderStep3() {
 
 function step3Next() {
     // check inputs
-    let emptyText = new Set();
+    let valid = true;
     for (let i = 0; i < data.links.length; i++) {
-        // check for empty state names
-        if (data.links[i].name.length === 0) {
-            emptyText.add(data.links[i].id);
+        const input = document.getElementById("link-input-" + data.links[i].source.id + "-" + data.links[i].target.id);
+        if (data.links[i].rate === undefined || input.value.length === 0) {
+            data.links[i].rate = undefined;
+            valid = false;
+            input.className += " border border-danger"
         }
     }
-    const noRate = Array.from(emptyText);
-    for (let i = 0; i < noRate.length; i++) {
-        const element = document.getElementById('s-3-input-' + noRate[i]);
-        element.className += " border border-danger"
-    }
 
-    if (noRate.length === 0 && data.links.length > 0) {
+    if (valid) {
         step++;
-        const element = document.getElementById("step3-container");
-        element.style.display = "none";
+        const step3 = document.getElementById("step3-container");
+        const step4 = document.getElementById("step4-container");
+        step3.style.display = "none";
+        step4.style.display = "flex";
+        formTitle.innerHTML = "Data Input (Step 4)";
         renderStep4();
     }
 }
@@ -495,26 +542,29 @@ function step3Next() {
 
 function renderStep4() {
     const step4 = document.getElementById("step4-container");
-    step4.replaceChildren();
-    
-    const selectSource = document.createElement("select");
-    selectSource.className = "form-select";
-    selectSource.ariaLabel = "Select Source Node";
-    selectSource.id = "selectSource";
-    
+    const stepTitle = document.createElement("h3");
+    stepTitle.className = "title";
+    stepTitle.innerHTML = "Inducer-Based Transition Rates"
+    step4.replaceChildren(stepTitle);
+
     const sourceLabel = document.createElement("p");
     sourceLabel.innerHTML = "Select Source Node:";
 
     const targetLabel = document.createElement("p");
     targetLabel.innerHTML = "Select Target Node:";
-    
-    const edgeLabel = document.createElement("p");
-    edgeLabel.innerHTML = "Select Edge Node:";
-    
+
+    const inducerLabel = document.createElement("p");
+    inducerLabel.innerHTML = "Select Inducer Node:";
+
+    const selectSource = document.createElement("select");
+    selectSource.className = "form-select";
+    selectSource.ariaLabel = "Select Source Node";
+    selectSource.id = "selectSource";
+
     for (let i = 0; i < data.nodes.length; i++) {
         const option = document.createElement("option");
         option.value = data.nodes[i].id;
-        option.innerHTML = data.nodes[i].name;  
+        option.innerHTML = data.nodes[i].name;
         option.id = "select-source-" + data.nodes[i].id;
         selectSource.appendChild(option);
     }
@@ -523,7 +573,7 @@ function renderStep4() {
     selectTarget.className = "form-select";
     selectTarget.ariaLabel = "Select Target Node";
     selectTarget.id = "selectTarget";
-    
+
     for (let i = 0; i < data.nodes.length; i++) {
         const option = document.createElement("option");
         option.value = data.nodes[i].id;
@@ -532,25 +582,117 @@ function renderStep4() {
         selectTarget.appendChild(option);
     }
 
-    const selectEdge = document.createElement("select");
-    selectEdge.className = "form-select";
-    selectEdge.ariaLabel = "Select Edge Node";
-    selectEdge.id = "selectEdge";
-    
+    const selectInducer = document.createElement("select");
+    selectInducer.className = "form-select";
+    selectInducer.ariaLabel = "Select Inducer Node";
+    selectInducer.id = "selectInducer";
+
     for (let i = 0; i < data.nodes.length; i++) {
         const option = document.createElement("option");
         option.value = data.nodes[i].id;
-        option.innerHTML = data.nodes[i].name; 
-        option.id = "select-edge-" + data.nodes[i].id;
-        selectEdge.appendChild(option);
+        option.innerHTML = data.nodes[i].name;
+        option.id = "select-inducer-" + data.nodes[i].id;
+        selectInducer.appendChild(option);
     }
+
+    const rateAndAddContainer = document.createElement("div");
+    rateAndAddContainer.style.display = "flex";
+    rateAndAddContainer.style.justifyContent = "space-between";
+
+    const rateInputGroup = document.createElement("div");
+    rateInputGroup.className = "input-group";
+    rateInputGroup.style.width = "60%";
+
+    const rateInput = document.createElement("input");
+    rateInput.type = "number";
+    rateInput.className = "form-control";
+    rateInput.placeholder = "Enter Trans. Rate";
+    rateInput.ariaLabel = "Enter Trans. Rate";
+
+    rateInputGroup.appendChild(rateInput);
+
+    const addButton = document.createElement("button");
+    addButton.className = "btn btn-success";
+    addButton.innerHTML = "Add";
+    addButton.style.width = "30%";
+    addButton.onclick = () => {
+        let valid = true
+        if (selectSource.value === selectTarget.value) {
+            valid = false
+            selectSource.className += " border border-danger";
+            selectTarget.className += " border border-danger";
+        } else {
+            selectSource.classList.remove("border");
+            selectSource.classList.remove("border-danger");
+            selectTarget.classList.remove("border");
+            selectTarget.classList.remove("border-danger");
+        }
+
+        if (rateInput.value.length === 0) {
+            valid = false
+            rateInput.className += " border border-danger";
+        } else {
+            rateInput.classList.remove("border");
+            rateInput.classList.remove("border-danger");
+        }
+
+
+        if (valid && data.links.find(link => link.source.id === parseInt(selectSource.value) && link.target.id === parseInt(selectTarget.value) && link.inducer === parseInt(selectInducer.value)) === undefined) {
+            console.log(data.links);
+            // TODO: show alert when tries to add an existing link
+            data.links.push({
+                source: parseInt(selectSource.value),
+                target: parseInt(selectTarget.value),
+                inducer: parseInt(selectInducer.value),
+                rate: rateInput.value
+            })
+            const edge = document.createElement("div");
+            edge.innerHTML = selectSource.value + selectTarget.value + selectInducer.value + rateInput.value;
+            const deleteButton = document.createElement("button");
+            deleteButton.className = "btn btn-danger";
+            deleteButton.innerHTML = "X";
+            deleteButton.style.width = "25%";
+            deleteButton.onclick = () => {
+            }
+            Graph.graphData(data);
+        }
+
+
+    }
+
+    rateAndAddContainer.appendChild(rateInputGroup);
+    rateAndAddContainer.appendChild(addButton);
 
     step4.appendChild(sourceLabel);
     step4.appendChild(selectSource);
     step4.appendChild(targetLabel);
     step4.appendChild(selectTarget);
-    step4.appendChild(edgeLabel);
-    step4.appendChild(selectEdge);
-    
+    step4.appendChild(inducerLabel);
+    step4.appendChild(selectInducer);
+    step4.appendChild(rateAndAddContainer);
+    step4.appendChild(edge);
+
     Graph.graphData(data);
+}
+
+function renderStep5() {
+    const step5 = document.getElementById("step5-container");
+    const stepTitle = document.createElement("h3");
+    stepTitle.className = "title";
+    stepTitle.innerHTML = "State Transition Rates";
+    step5.replaceChildren(stepTitle);
+
+    for (let i = 0; i < data.links.length; i++) {
+        const fromState = data.links[i].source.name;
+        const toState = data.links[i].target.name;
+        let inducerState = "None";
+        if (data.links[i].inducer !== undefined) {
+            inducerState = data.links[i].inducer.name;
+        }
+        const rate = data.links[i].rate;
+
+        const text = document.createElement("p");
+        text.innerHTML = fromState + "&#09;" + toState + "&#09;" + inducerState + "&#09;" + rate;
+        step5.appendChild(text);
+    }
 }
